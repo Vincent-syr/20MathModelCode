e9af34cda22a02b0558c4cd532c1fc1554ed632f
Vincent-syr <583636762@qq.com>
2020-09-20 16:00:46 +0800

    commit

--- train_subject.py
+++ train_subject.py
@@ -4,6 +4,7 @@ import torch
 import torch.nn as nn
 import matplotlib.pyplot as plt
 import numpy as np
+import copy
 import os
 from os.path import join
 from model2 import ConvNet
@@ -15,10 +16,10 @@ is_with_ave = sys.argv[3]  # w; o
 test_size = 12
 data_path = "./data"
 batch_size = 48
-epoch = 1500
+epoch = 1000
 learning_rate = 1e-3
 weight_decay = 1e-2
-loss_weight = 3
+loss_weight = 4.5
 channel_begin = 0
 channel_end = 20
 print_frep = 50
@@ -27,7 +28,6 @@ print_frep = 50
 def minibatch(data, batch_size):
     start = 0
     while True:
-
         end = start + batch_size
         yield data[start:end]
 
@@ -80,7 +80,7 @@ def weighted_BCE(predictions, labels):
     predictions = predictions.clamp(1e-6, 1 - 1e-6)
     labels = labels.clamp(0, 1)
     # loss = -labels * torch.log(predictions) - 5 * (1 - labels) * torch.log(1 - predictions)
-    loss = -loss_weight * labels * torch.log(predictions) - (1 - labels) * torch.log(1 - predictions)
+    loss = - loss_weight * labels * torch.log(predictions) - (1 - labels) * torch.log(1 - predictions)
 
     return loss.mean()
 
@@ -159,6 +159,9 @@ def train(train_data, test_data):
     # test_data = data[cutoff:]
 
     # shuffle data
+    test_data_size = len(test_data)
+    copy_train_data = copy.deepcopy(train_data)
+    copy_train_size = len(copy_train_data)
     train_data_size = len(train_data)
     shuffle_idx = np.random.permutation(train_data_size)
     train_data = train_data[shuffle_idx]
@@ -181,14 +184,26 @@ def train(train_data, test_data):
 
     collect_train_acc = []
     collect_val_acc = []
+    collect_loss = []
+
     collect_test_acc = []
     collect_test_precision = []
     collect_test_recall = []
     collect_test_fscore = []
-
     collect_rc_total = []
     collect_correct_total = []
     collect_corr_num = []
+
+    collect_copy_train_acc = []
+    collect_copy_train_precision = []
+    collect_copy_train_recall = []
+    collect_copy_train_fscore = []
+    collect_copy_train_rc_total = []
+    collect_copy_train_correct_total = []
+    collect_copy_train_corr_num = []
+
+
+
     
     for i in range(k_fold):
         # split data into train/val
@@ -216,6 +231,7 @@ def train(train_data, test_data):
                 count += 1
                 train_pred.append(train_batch_pred)
             losses /= count
+            collect_loss.append(losses)
             train_pred = np.concatenate(train_pred, axis=0)
 
             val_pred = []
@@ -249,7 +265,7 @@ def train(train_data, test_data):
             # test acc
             model = model.eval()
             test_pred = []
-
+            train_pred = []
             rc_total = 0
             correct_total = 0
             pred_values = []
@@ -274,10 +290,6 @@ def train(train_data, test_data):
                 correct_total += corr
                 test_pred.append(test_batch_pred)
 
-            # if corr_num >= 1:
-            #     # 保存模型
-            #     save_ckpt(i, model, optimizer, is_nice=True)
-
             test_pred = np.concatenate(test_pred, axis=0)
             test_target = test_data[:, 1].reshape(-1)
             test_acc = cal_acc(test_pred, test_target)
@@ -287,25 +299,88 @@ def train(train_data, test_data):
 
             if (curr_epoch + 1) % print_frep == 0:
                 # test_acc: binary acc, rc_correct: 12行列对1个； complete_correct: 12行列对2个; final: pass
-                print(f"TestSet! fold: {i + 1}, epoch: {curr_epoch + 1}, test acc: {test_acc}, rc correct: {rc_total}/20, complete correct: {correct_total}/10, final: {corr_num}/2")
+                print(f"TestSet! fold: {i + 1}, epoch: {curr_epoch + 1}, test acc: {test_acc}, rc correct: {rc_total}/{int(test_data_size*2/12)}, complete correct: {correct_total}/{int(test_data_size/12)}, final: {corr_num}/2")
                 print(f"TestSet! fold: {i + 1}, epoch: {curr_epoch + 1}, test precision: {test_precision}, test recall: {test_recall}, test f score: {test_f_score}")
 
             collect_test_acc.append(test_acc)
             collect_test_precision.append(test_precision)
             collect_test_recall.append(test_recall)
             collect_test_fscore.append(test_f_score)
-
             collect_rc_total.append(rc_total)
             collect_correct_total.append(correct_total)
             collect_corr_num.append(corr_num)
 
+            # if corr_num >= 1:
+            #     # 保存模型
+            #     save_ckpt(i, model, optimizer, is_nice=True)
+
+            # copy train
+            copy_train_pred = []
+            rc_total = 0
+            correct_total = 0
+            pred_values = []
+            corr_num = 0
+            for c in minibatch(copy_train_data, test_size):
+                copy_train_batch_pred, pred_value, y_sort, rc_count, corr = test_batch(model, criterion, optimizer, c)
+                pred_values.append(pred_value)
+
+                if len(pred_values) == 5:
+                    mean_value = np.mean(np.array(pred_values), axis=0)
+                    row_corr = np.argmax(y_sort[:test_size // 2]) == np.argmax(mean_value[:test_size // 2])
+                    col_corr = np.argmax(y_sort[test_size // 2:]) == np.argmax(mean_value[test_size // 2:])
+                    # print(mean_value)
+                    # print(row_corr)
+                    # print(col_corr)
+                    corr_num += int(row_corr and col_corr)
+                    pred_values = []
+
+                assert rc_count in [0, 1, 2]
+                assert corr in [0, 1]
+                rc_total += rc_count
+                correct_total += corr
+                copy_train_pred.append(copy_train_batch_pred)
+
+            copy_train_pred = np.concatenate(copy_train_pred, axis=0)
+            copy_train_target = copy_train_data[:, 1].reshape(-1)
+            copy_train_acc = cal_acc(copy_train_pred, copy_train_target)
+            # print("test_pred:", test_pred)
+            # print("test_targ:", test_target)
+            copy_train_f_score, copy_train_precision, copy_train_recall = cal_f(copy_train_pred, copy_train_target)
+
+            if (curr_epoch + 1) % print_frep == 0:
+                # test_acc: binary acc, rc_correct: 12行列对1个； complete_correct: 12行列对2个; final: pass
+                print(f"CopyTrainSet! fold: {i + 1}, epoch: {curr_epoch + 1}, copy_train_acc  acc: {copy_train_acc }, rc correct: {rc_total}/{int(copy_train_size*2/12)}, complete correct: {correct_total}/{int(copy_train_size/12)}, final: {corr_num}/2")
+                print(f"CopyTrainSet! fold: {i + 1}, epoch: {curr_epoch + 1}, copy_train precision: {copy_train_precision}, copy_train recall: {copy_train_recall}, copy_train f score: {copy_train_f_score}")
+
+
+            collect_copy_train_acc.append(copy_train_acc)
+            collect_copy_train_precision.append(copy_train_precision)
+            collect_copy_train_recall.append(copy_train_recall)
+            collect_copy_train_fscore.append(copy_train_f_score)
+
+            collect_copy_train_rc_total.append(rc_total)
+            collect_copy_train_correct_total.append(correct_total)
+            collect_copy_train_corr_num.append(corr_num)
+
     save_ckpt(i, model, optimizer, is_nice=False)
 
     plt.figure()
-    plt.plot(range(len(collect_rc_total)), collect_rc_total, label='rc_total')
-    plt.plot(range(len(collect_correct_total)), collect_correct_total, label='correct_total')
+    plt.title('Final acc')
+    plt.plot(range(epoch), np.array(collect_copy_train_rc_total)/100.0, label='train_rc_total')
+    plt.plot(range(epoch), np.array(collect_copy_train_correct_total)/50.0, label='train_correct_total')
+    plt.plot(range(epoch), np.array(collect_rc_total)/20.0, label='test_rc_total')
+    plt.plot(range(epoch), np.array(collect_correct_total)/10.0, label='test_correct_total')
     plt.legend()
+    plt.savefig('final_acc.png')
     plt.show()
+
+    # plt.figure()
+    # plt.title('Test acc, Total %d' % int(len(test_data)/12))
+    # plt.plot(range(len(collect_rc_total)), collect_rc_total, label='rc_total')
+    # plt.plot(range(len(collect_correct_total)), collect_correct_total, label='correct_total')
+    # plt.legend()
+    # plt.savefig('test_acc.png')
+    # plt.show()
 
     plt.figure()
     plt.title('Test index')
@@ -313,9 +388,23 @@ def train(train_data, test_data):
     plt.plot(range(epoch), collect_test_precision, label='precision')
     plt.plot(range(epoch), collect_test_recall, label='recall')
     plt.plot(range(epoch), collect_test_fscore, label='fscore')
+    plt.legend()
+    plt.savefig('test_index.png')
     plt.show()
     
     plt.figure()
+    plt.title('Train loss')
+    plt.plot(range(epoch), collect_loss)
+    plt.savefig('train_loss.png')
+    plt.show()
+
+    # plt.figure()
+    # plt.title('Leaning rate')
+    # plt.plot(range(epoch), collect_loss)
+    # plt.savefig('train_loss.png')
+    # plt.show()
+
+
 
     average_train_acc = np.mean(np.array(collect_train_acc))
     average_val_acc = np.mean(np.array(collect_val_acc))
@@ -370,7 +459,7 @@ if __name__ == '__main__':
             data_train = np.vstack((data_train, temp[:-120]))
             data_val = np.vstack((data_val, temp[-120:]))  # 2个字符
         # print
-        train(data_train, data_val)
+        train(data_train, data_val)  # 600, 120
     else:
         # test(data)
         pass
